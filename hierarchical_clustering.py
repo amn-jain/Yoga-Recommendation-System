d93af99c6835524a5fcdc12361a21386c9dd7021
PadamSharma <56642812+PadamSharma@users.noreply.github.com>
2021-08-03 23:54:27 +0530

    Add files via upload

---
+++ hierarchical_clustering.py
@@ -0,0 +1,8 @@
+import json
+import random
+
+with open('../Machine_Learning/sample.json') as json_file:
+    data = json.load(json_file)
+    for i in range(10):
+      k = random.randint(0, len(data[str(i)]) - 1)
+      print(data[str(i)][k])
